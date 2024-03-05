8e41a1269af9cb9aef6ab3c8ab767cf4bf299782
marwanmohamedd7 <maro.jk2345@gmail.com>
2024-03-05 04:34:15 +0200

    reusability updates

--- src/ui/SearchInput.jsx
+++ src/ui/SearchInput.jsx
@@ -2,7 +2,9 @@ function SearchInput({ placeholder }) {
     return (
         <input
             type="text"
-            className="shadow border py-2 px-3 mr-4 text-grey-darker text-sm placeholder:text-sm rounded-md w-64 focus:w-72 transition-all duration-300 focus:border-blue-600 focus:outline-none focus:ring-1 focus:ring-offset-1 focus:ring-blue-600"
+            className="shadow border py-2 px-3 mr-4 text-grey-darker text-sm
+             placeholder:text-sm rounded-md w-64 focus:w-72 transition-all duration-300
+             focus:border-blue-600 focus:outline-none focus:ring-1 focus:ring-offset-1 focus:ring-blue-600"
             placeholder={placeholder}
         />
     )
