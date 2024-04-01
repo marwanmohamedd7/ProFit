7599181d587cd2097b61e5b39c649a3c419724ea
marwanmohamedd7 <maro.jk2345@gmail.com>
2024-04-01 14:21:31 +0200

    img crop

--- src/ui/ProtectedRouteProfile.jsx
+++ src/ui/ProtectedRouteProfile.jsx
@@ -4,31 +4,30 @@ import { useEffect } from "react"
 import { useNavigate } from "react-router-dom"
 
 function ProtectedRouteProfile({ children, role }) {
-    // const navigate = useNavigate()
+    const navigate = useNavigate()
 
-    // // 1. Load the authenticated user
-    // const { isLoading, isAuthenticated, status } = useUser()
+    // 1. Load the authenticated user
+    const { isLoading, isAuthenticated, status } = useUser()
 
-    // // 2. If there is No authenticated user, redirect to login page
-    // useEffect(function () {
-    //     if (!isAuthenticated && !isLoading) navigate("/login", { replace: true });
-    //     if (isAuthenticated !== role && !isLoading) {
-    //         if (isAuthenticated === "admin") navigate("/admin", { replace: true });
-    //         else navigate("/login", { replace: true });
-    //     }
-    //     if (isAuthenticated === role && !isLoading && status === "pending") navigate("/login", { replace: true });
-    //     if (isAuthenticated === role && !isLoading && status === "accepted") navigate("/trainer", { replace: true });
-    //     // if (isAuthenticated === role && !isLoading && status === "accepted") navigate(-1, { replace: true });
-    // }, [isAuthenticated, isLoading, status, role, navigate])
+    // 2. If there is No authenticated user, redirect to login page
+    useEffect(function () {
+        if (!isAuthenticated && !isLoading) navigate("/login", { replace: true });
+        if (isAuthenticated !== role && !isLoading) {
+            if (isAuthenticated === "admin") navigate("/admin", { replace: true });
+            else navigate("/login", { replace: true });
+        }
+        if (isAuthenticated === role && !isLoading && status === "pending") navigate("/login", { replace: true });
+        if (isAuthenticated === role && !isLoading && status === "accepted") navigate("/trainer", { replace: true });
+        // if (isAuthenticated === role && !isLoading && status === "accepted") navigate(-1, { replace: true });
+    }, [isAuthenticated, isLoading, status, role, navigate])
 
-    // // 3. While loading, show a spinner
-    // if (isLoading) return <div className="h-dvh flex justify-center items-center">
-    //     <Spinner />
-    // </div>
+    // 3. While loading, show a spinner
+    if (isLoading) return <div className="h-dvh flex justify-center items-center">
+        <Spinner />
+    </div>
 
-    // // 4. If all checks pass, render the children components
-    // if (isAuthenticated === role && !isLoading && status === "incomplete") return children;
-return children
+    // 4. If all checks pass, render the children components
+    if (isAuthenticated === role && !isLoading && status === "incomplete") return children;
 }
 
 export default ProtectedRouteProfile
